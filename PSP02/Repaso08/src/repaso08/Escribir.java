095b1dc1cd4d73df074e13a7d9dc1fb0f3b91620
Iván Estévez <mskina@gmail.com>
2023-03-22 13:27:08 +0100

    Update Escribir.java

--- PSP02/Repaso08/src/repaso08/Escribir.java
+++ PSP02/Repaso08/src/repaso08/Escribir.java
@@ -4,9 +4,6 @@
  */
 package repaso08;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author Iván Estévez Sabucedo
@@ -15,7 +12,6 @@ public class Escribir extends Thread {
 
     private StringBuilder sb;
     private char c;
-
     private boolean escribiendo = false;
 
     public Escribir(StringBuilder sb, char c) {
@@ -23,25 +19,13 @@ public class Escribir extends Thread {
         this.c = c;
     }
 
-    public synchronized void guardarChar(char c) {
-        while (escribiendo) {
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                //
-            }
-        }
-        escribiendo = true;
-        for (int i = 0; i < 30; i++) {
-            sb.append(c);
-        }
-        escribiendo = false;
-        notifyAll();
-    }
-
     @Override
     public void run() {
-        guardarChar(c);
-    }
 
+        synchronized (this.sb) {
+            for (int i = 0; i < 30; i++) {
+                sb.append(c);
+            }
+        }
+    }
 }
