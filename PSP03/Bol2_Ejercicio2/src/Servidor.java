8f8b83b5800081870d968235814e7b61d57e3a9f
iestevez <ivanestevezsabucedo@gmail.com>
2023-05-10 15:55:40 +0200

    Fin Ej1

--- PSP03/Bol2_Ejercicio2/src/Servidor.java
+++ PSP03/Bol2_Ejercicio2/src/Servidor.java
@@ -1,7 +1,6 @@
 /**
  * Crea un programa servidor que reciba cadenas de caracteres, las muestre en pantalla y se las envíe al emisor en
- * mayúscula. El proceso servidor finalizará cuando reciba un asterisco.
- *
+ * mayúscula. El proceso servidor finalizará cuando reciba un asterisco. *
  * Establece un tiempo de espera de 5000ms con el método setSoTimeout para hacer que el método receive() del programa
  * cliente se bloquee. Pasado ese tiempo, controlar si no se reciben datos lanzando la excepción InterruptedIoException,
  * en cuyo caso visualiza un mensaje indicando que el paquete se ha perdido.
